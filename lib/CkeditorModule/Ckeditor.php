6239b953e73aa238a27a94b123fa2306713c8f37
lordlamer <lordlamer@lordlamer.de>
2012-12-06 23:19:12 +0100

    * using abstract rte class for ckeditor

--- lib/CkeditorModule/Ckeditor.php
+++ lib/CkeditorModule/Ckeditor.php
@@ -3,17 +3,20 @@
 /**
  * ckeditor class
  */
-class CkeditorModule_Ckeditor implements Knowledgeroot_Rte_Interface {
+class CkeditorModule_Ckeditor extends Knowledgeroot_Rte_Abstract {
 	/**
 	 * show editor with content
 	 *
 	 * @param string $content
 	 */
-	public function show($content) {
+	public function show($content = null) {
+	    if($content != null)
+		$this->content = (string) $content;
+
 	    $res = "";
 	    $res .= '<script src="./module/ckeditor/ckeditor/ckeditor.js"></script>';
-	    $res .= '<textarea name="content">'.htmlspecialchars($content).'</textarea>';
-	    $res .= "<script>CKEDITOR.replace('content');</script>";
+	    $res .= '<textarea name="' . $this->name . '">'.htmlspecialchars($this->content).'</textarea>';
+	    $res .= "<script>CKEDITOR.replace('" . $this->name . "');</script>";
 
 	    return $res;
 	}
